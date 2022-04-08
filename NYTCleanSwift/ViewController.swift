c0f63c45b278d86ee3ed067f519fdda60e727af7
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2022-04-08 09:34:37 +0300

    Initial Commit

---
+++ NYTCleanSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYTCleanSwift
+//
+//  Created by Abdulrahman on 4/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
